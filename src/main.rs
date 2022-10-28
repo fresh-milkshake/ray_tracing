fa2fa030a4898b1732cd6936b76f05a3acf50064
immacool <superdeveloper43@gmail.com>
2022-10-28 13:09:02 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 mod image;
 mod object;
 
-use std::f64::consts::PI;
-
 use image::Image;
-use nalgebra::Vector3;
-
 use object::Light;
 use object::Material;
 use object::Sphere;
-// use rand::random;
+
+use std::f64::consts::PI;
+
+use futures;
+use futures::executor::block_on;
+use nalgebra::Vector3;
+
 
 const BACKGROUND_COLOR: Vector3<f64> = Vector3::new(0.7, 0.8, 1.0);
 const MAX_RECURSION_DEPTH: u32 = 6;
 
+/// Returning the reflection of the vector `i` on the normal `n`
+///
+/// ### Arguments
+///
+/// * `i` - The incident vector
+/// * `n` - The normal vector
+///
+/// ### Returns
+///
+/// Vector3<f64> - The reflected vector
+///
 fn reflect(i: Vector3<f64>, n: Vector3<f64>) -> Vector3<f64> {
     return i - n * 2.0 * (i.dot(&n));
 }
@@ -42,16 +55,20 @@ fn scene_intersect(
     let mut n = Vector3::default();
     let mut material = Material::default();
 
+    // initialize minimum distance to max value of f64 (infinity used for comparison)
     let mut spheres_dist = std::f64::MAX;
+    // iterate over all spheres in the scene
+    // and evaluate the intersection with the ray
+    // to get all its properties
     for sphere in spheres {
-        let mut dist_i = 0.0;
+        let mut dist_i = 0.0; // distance to intersection
         let (is_intersect, dist_i0) = sphere.ray_intersect(ray_origin, ray_direction, dist_i);
         dist_i = dist_i0;
         if is_intersect && dist_i < spheres_dist {
-            spheres_dist = dist_i;
-            point = ray_origin + ray_direction * dist_i;
-            n = (point - sphere.center).normalize() as Vector3<f64>;
-            material = sphere.material.clone();
+            spheres_dist = dist_i; // update minimum distance with the current distance
+            point = ray_origin + ray_direction * dist_i; // get the intersection point
+            n = (point - sphere.center).normalize() as Vector3<f64>; // change the normal to point to center of the sphere
+            material = sphere.material.clone(); // get material of the sphere
         }
     }
     Some((spheres_dist < 1000.0, point, n, material))
@@ -83,11 +100,18 @@ fn cast_ray(
     lights: &Vec<Light>,
     depth: u32,
 ) -> Vector3<f64> {
+    // check if the ray intersects any object
+    // if it does, compute the intersection point, the normal and the color
+    // if it doesn't, or if the maximum recursion depth has been reached (to avoid infinite recursion
+    // when the ray hits the mirror surface), return the background color
     let (is_intersect, point, n, material) = scene_intersect(origin, direction, spheres).unwrap();
     if !is_intersect || depth > MAX_RECURSION_DEPTH {
         return BACKGROUND_COLOR;
     }
 
+    // compute the reflection direction (not need to normalize because all vectors are already
+    // normalized) and the color of the reflected ray (recursive call, cuz the reflected ray can
+    // also reflect on other surfaces)
     let reflect_direction = reflect(direction, n);
     let reflect_origin = if reflect_direction.dot(&n) < 0.0 {
         point - n * 1e-3
@@ -102,28 +126,37 @@ fn cast_ray(
         depth + 1,
     );
 
+    // compute color diffused by lambertian shading
+    // lambertian shading is the simplest and most common shading model:
+    // the color of a point is proportional to the cosine of the angle between the normal and the
+    // light vector
     let mut diffuse_light_intensity = 0.0;
     let mut specular_light_intensity = 0.0;
     for light in lights {
         let light_direction = (light.position - point).normalize();
         let light_distance = (light.position - point).norm();
 
-        // Check if the point lies in the shadow of the current light
+        // evaluate shadow origin and direction to check if the point is in shadow
         let shadow_origin = if light_direction.dot(&n) < 0.0 {
             point - n * 1e-3
         } else {
             point + n * 1e-3
         };
 
+        // Check if the point lies in the shadow of the current light
+        // If it does, skip this light
+        // If it doesn't, add the contribution of the light to the diffuse and specular light
         let (shadow_intersect, shadow_pt, _, _) =
             scene_intersect(shadow_origin, light_direction, spheres).unwrap();
         if shadow_intersect && (shadow_pt - shadow_origin).norm() < light_distance {
             continue;
         }
 
+        // add the contribution of the light to color diffusing
         let max_light = nalgebra::clamp(light_direction.dot(&n), 0.0, 1.0);
         diffuse_light_intensity += light.intensity * max_light;
 
+        // process specular light
         let minus_ref = reflect(light_direction, n).dot(&direction);
         let power = nalgebra::clamp(minus_ref, 0.0, 1.0);
         specular_light_intensity += power.powf(material.specular_exponent) * light.intensity;
@@ -134,57 +167,58 @@ fn cast_ray(
     diffuse_color
 }
 
-/// Render a scene with spheres and lights
-fn render(spheres: &Vec<Sphere>, lights: &Vec<Light>) {
-    // size of the resulting image in pixels
-    let width: u32 = 1024;
-    let height: u32 = 768;
-    // field of view in radians (90 degrees)
-    let fov: f64 = PI / 2.0;
+/// Asyncronous version of the `cast_ray` function with same arguments
+/// except for the `i` and `j` arguments which are used to write the
+/// pixel color to the image buffer
+async fn cast_ray_async(
+    origin: Vector3<f64>,
+    direction: Vector3<f64>,
+    spheres: &Vec<Sphere>,
+    lights: &Vec<Light>,
+    depth: u32,
+    i: u32,
+    j: u32,
+) -> (u32, u32, Vector3<f64>) {
+    (i, j, cast_ray(origin, direction, spheres, lights, depth))
+}
 
-    println!("Generating image...");
-    // `framebuffer` is a 1D array of pixels (3D vectors, containing RGB values) that will be rendered
-    // to an image. It is indexed as `framebuffer[y * width + x]` (row-major order).
-    let mut framebuffer = vec![Vector3::new(0.0, 0.0, 0.0); (width * height) as usize];
-    // go through each pixel of the image and cast a ray through it to
-    // find the color of the pixel
+/// Render a scene with spheres and lights
+async fn render(
+    width: u32,
+    height: u32,
+    fov: f64,
+    spheres: &Vec<Sphere>,
+    lights: &Vec<Light>,
+) -> Vec<u8> {
+    // `buffer` is a 1D array of pixels (RGB triplets) with the size of the image
+    let mut buffer = vec![0; (width * height * 3) as usize];
+    let mut tasks = Vec::new();
+
     for j in 0..height {
         for i in 0..width {
-            print!("\rScanlines remaining: {}\r", height - j - 1);
-            std::io::Write::flush(&mut std::io::stdout()).unwrap();
             // X and Y calculated from the camera's perspective by the formula
             // x = (2 * (i + 0.5) / width - 1) * tan(fov / 2) * width / height
             // y = -(2 * (j + 0.5) / height - 1) * tan(fov / 2)
             // z = -1
-            let x: f64 =
+            let x =
                 (2.0 * (i as f64 + 0.5) / width as f64 - 1.0) * (fov / 2.0).tan() * width as f64
                     / height as f64;
-            let y: f64 = -(2.0 * (j as f64 + 0.5) / height as f64 - 1.0) * (fov / 2.0).tan();
+            let y = -(2.0 * (j as f64 + 0.5) / height as f64 - 1.0) * (fov / 2.0).tan();
             // The camera is at (0, 0, 0) and looks along the negative Z axis
             // The direction of the ray is the normalized vector from the camera to the pixel
-            let dir: Vector3<f64> = Vector3::new(x, y, -1.0).normalize();
-            framebuffer[(j * width + i) as usize] =
-                cast_ray(Vector3::new(0.0, 0.0, 0.0), dir, spheres, lights, 0);
+            let direction = Vector3::new(x, y, -1.0).normalize();
+            let task = cast_ray_async(Vector3::default(), direction, spheres, lights, 0, i, j);
+            tasks.push(task);
         }
     }
-
-    // save framebuffer to file (.png format) via Image struct
-    println!("Saving framebuffer to file");
-    let mut image = Image::new(width, height);
-    for j in 0..height {
-        for i in 0..width {
-            let pixel_index: usize = (j * width + i) as usize;
-            // convert framebuffer values from floats to 0-255 integers
-            // by multiplying by 255 and clamping to [0, 255]
-            let color: Vec<u8> = vec![
-                (255.0 * framebuffer[pixel_index].x) as u8,
-                (255.0 * framebuffer[pixel_index].y) as u8,
-                (255.0 * framebuffer[pixel_index].z) as u8,
-            ];
-            image.set_pixel(i, j, color);
-        }
+    let results = futures::future::join_all(tasks).await;
+    for (i, j, color) in results {
+        let index = (i + j * width) as usize;
+        buffer[index * 3] = (color.x * 255.0) as u8;
+        buffer[index * 3 + 1] = (color.y * 255.0) as u8;
+        buffer[index * 3 + 2] = (color.z * 255.0) as u8;
     }
-    image.save("image.png");
+    buffer
 }
 
 fn main() {
@@ -204,29 +238,6 @@ fn main() {
         1425.0,
     );
 
-    // const SPHERES_COUNT: i32 = 3;
-    // generate spheres_count random spheres in the field of view
-    // let mut spheres = Vec::new();
-    // for _ in 0..SPHERES_COUNT {
-    //     // choose x, y, z coordinates randomly, but make sure that the sphere is
-    //     // inside the field of view
-    //     let x = random::<f64>() * 2.0 - 1.0;
-    //     let y = random::<f64>() * 2.0 - 1.0;
-    //     let z = random::<f64>() * 2.0 - 1.0;
-    //     let radius: f64 = random::<f64>() * 1.0 + 0.5;
-    //     let material: Material;
-    //     // generate a random integer between 0 and 2 and choose a material
-    //     let choose_mat: i32 = random::<i32>() % 3;
-    //     if choose_mat == 0 {
-    //         material = ivory;
-    //     } else if choose_mat == 1 {
-    //         material = red_rubber;
-    //     } else {
-    //         material = mirror;
-    //     }
-    //     spheres.push(Sphere::new(Vector3::new(x, y, z), radius, material));
-    // }
-
     let spheres = vec![
         Sphere::new(Vector3::new(-3.0, 0.0, -16.0), 2.0, ivory),
         Sphere::new(Vector3::new(-1.0, -1.5, -12.0), 2.0, red_rubber),
@@ -240,5 +251,23 @@ fn main() {
         Light::new(Vector3::new(30.0, 20.0, 30.0), 1.7),
     ];
 
-    render(&spheres, &lights);
+    // size of resulting image
+    let (width, height) = (1024, 768);
+    // field of view in radians (90 degrees)
+    let fov = PI / 2.0;
+    let framebuffer: Vec<u8> = block_on(render(width, height, fov, &spheres, &lights));
+
+    let mut image = Image::new(width, height);
+    for j in 0..height {
+        for i in 0..width {
+            let pixel_index: usize = (j * width + i) as usize;
+            let color: Vec<u8> = vec![
+                framebuffer[pixel_index * 3],
+                framebuffer[pixel_index * 3 + 1],
+                framebuffer[pixel_index * 3 + 2],
+            ];
+            image.set_pixel(i, j, color);
+        }
+    }
+    image.save("out.png");
 }
--- src/object.rs
+++ src/object.rs
@@ -1,6 +1,3 @@
-use std::fmt::Formatter;
-use core::fmt::Debug;
-
 use nalgebra::Vector3;
 
 #[derive(Clone, Copy)]
@@ -27,14 +24,6 @@ impl Material {
             albedo,
             specular_exponent
         }
-    }
-}
-
-impl Debug for Material {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Material")
-            .field("diffuse_color", &self.diffuse_color)
-            .finish()
     }
 }
 
@@ -65,33 +54,6 @@ impl Sphere {
     /// 
     /// bool - Whether the ray intersects the sphere or not
     /// 
-    /// ### Notes
-    /// 
-    /// The ray is defined by the parametric equation:
-    /// 
-    /// `P(t) = ray_origin + t * ray_direction`
-    /// 
-    /// The sphere is defined by the equation:
-    /// 
-    /// `(P(t) - center) * (P(t) - center) = radius * radius`
-    /// 
-    /// ### Example
-    /// 
-    /// ```
-    /// use nalgebra::Vector3;
-    /// use raytracer::Sphere;
-    /// 
-    /// let sphere = Sphere::new(Vector3::new(0.0, 0.0, 0.0), 1.0);
-    /// let ray_origin = Vector3::new(0.0, 0.0, 0.0);
-    /// let ray_direction = Vector3::new(0.0, 0.0, 1.0);
-    /// 
-    /// let t0 = 0.0;
-    /// let t1 = 0.0;
-    /// 
-    /// let intersects = sphere.intersect(ray_origin, ray_direction, &mut t0, &mut t1);
-    /// 
-    /// assert_eq!(intersects, true);
-    /// ```
     #[warn(unused_assignments)]
     pub fn ray_intersect(&self, ray_origin: Vector3<f64>, dir: Vector3<f64>, mut t0: f64) -> (bool, f64) {
         let l: Vector3<f64> = self.center - ray_origin;
@@ -106,13 +68,6 @@ impl Sphere {
         (true, t0)
     }
 }
-
-impl Debug for Sphere {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
-        write!(f, "Sphere {{ center: {:?}, radius: {:?}, material: {:?} }}", self.center, self.radius, self.material)
-    }
-}
-
 
 pub struct Light {
     pub position: Vector3<f64>,
